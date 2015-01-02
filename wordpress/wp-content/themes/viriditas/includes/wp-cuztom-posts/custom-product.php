d5746e808d6de83e3c300320e00a380ee123cc7a
ajay <ajay786singh@gmail.com>
2015-01-02 22:02:34 +0530

    remove action custom taxonomy from products section

--- wordpress/wp-content/themes/viriditas/includes/wp-cuztom-posts/custom-product.php
+++ wordpress/wp-content/themes/viriditas/includes/wp-cuztom-posts/custom-product.php
@@ -2,5 +2,5 @@
 //Create article custom post type
 $products = new Cuztom_Post_Type('product');
 //$products->add_taxonomy( 'Body system' );
-$products->add_taxonomy( 'Actions' );
+//$products->add_taxonomy( 'Actions' );
 ?>
