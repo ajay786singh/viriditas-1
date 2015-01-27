edbc40ee078501578d12f3973d2a0d04b2bf3c5f
ajay <ajay786singh@gmail.com>
2015-01-27 16:58:39 +0530

    style fix

--- wordpress/wp-content/themes/viriditas/src/js/custom/menu-function.js
+++ wordpress/wp-content/themes/viriditas/src/js/custom/menu-function.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function() {
 	var menu = new Menu;
+	
+	jQuery(window).scroll(function() {		
+		var banner_height=jQuery('section[role=banner]').height();		
+		if (jQuery(this).scrollTop() > banner_height-100){  		
+			jQuery('header').addClass("active-header");		
+		}		
+		else{		
+			jQuery('header').removeClass("active-header");		
+		}		
+	});
+
 });
