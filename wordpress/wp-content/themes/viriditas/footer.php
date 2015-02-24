b302d827bfe7ec3723afc5627f0a84d4f71b932a
ajay <ajay786singh@gmail.com>
2015-02-24 18:16:20 +0530

    home page style add

--- wordpress/wp-content/themes/viriditas/footer.php
+++ wordpress/wp-content/themes/viriditas/footer.php
@@ -2,46 +2,46 @@
 <!-- Wrapper Ends Here -->
     <footer>
 		<div class="container">
-			<div class="span-11 center">
-				<div class="span-6 footer-logo">
-					<img src="<?php bloginfo('template_url');?>/dist/images/logo@2x.png">
-					<div class="newsletter-box">
-						<h6>Viriditas</h6>
-						<p>Echo Park ugh umami pug messenger bag.</p>
-						<form action="#" method="post">
-							<input type="hidden" value="2" name="group[2][2]" id="mce-group[2]-2-1">
-							<fieldset>
-								<ol>
-									<li><input name="EMAIL" placeholder="you@yourdomain.com" type="email" value=""  id="mce-EMAIL" required></li>
-									<li><input type="submit" value="Yes"></li>
-								</ol>
-							</fieldset>
-					  </form>
+			<div class="span-6 footer-logo">
+				<img src="<?php bloginfo('template_url');?>/dist/images/footer-logo.png">
+				<div class="newsletter-box">
+					<h5>Viriditas</h5>
+					<p>Echo Park ugh umami pug messenger bag.</p>
+					<form action="#" method="post">
+						<input type="hidden" value="2" name="group[2][2]" id="mce-group[2]-2-1">
+						<fieldset>
+							<ol>
+								<li><input name="EMAIL" placeholder="you@yourdomain.com" type="email" value=""  id="mce-EMAIL" required></li>
+								<li><input type="submit" value="Submit"></li>
+							</ol>
+						</fieldset>
+					</form>
+					<div class="copyright">
+						<p>copyright Viriditas <?php echo date('Y');?></p>
+						<p><a href="">Privacy policy</a> | <a href="">Terms of condition</a></p>
 					</div>
+					
+				</div>
+			</div>
+			<div class="span-6">
+				<div class="span-6">
+					<h6>Resources</h6>
+					<p>Austin 3 wolf moon disrupt church-key stumptown butcher swag. XOXO health goth  Austin, messenger bag photo booth migas plaid post-ironic church-key. Direct trade Carles health goth four dollar toast.</p>
 				</div>
 				<div class="span-6">
-					<div class="span-6">
-						<h6>Resources</h6>
-						<p>Austin 3 wolf moon disrupt church-key stumptown butcher swag. XOXO health goth  Austin, messenger bag photo booth migas plaid post-ironic church-key. Direct trade Carles health goth four dollar toast.</p>
-					</div>
-					<div class="span-6">
-						<h6>Contact</h6>
-						<p>
-							2775 Dundas St. West<br>
-							Toronto, ON, M6P 1Y4<br>
-							Tel: 416-767-3428<br>
-							Fax: 416-767-1215<br>
-							<a href="mailto: redden@viriditasherbalproducts.com">redden@viriditasherbalproducts.com</a>
-						</p>
-					</div>
+					<h6>Contact Us</h6>
+					<p>
+						2775 Dundas St. West<br>
+						Toronto, ON, M6P 1Y4<br>
+						Tel: 416-767-3428<br>
+						Fax: 416-767-1215<br>
+						<a href="mailto: redden@viriditasherbalproducts.com">redden@viriditasherbalproducts.com</a>
+					</p>
 				</div>
 			</div>
 		</div>
     </footer>
-<?php /*<script src="<?php bloginfo('template_url' ); ?>/bower_components/jquery/dist/jquery.min.js"></script> 
-<script src="<?php bloginfo('template_url' ); ?>/js/app.min.js"></script>*/?>
 <?php wp_footer(); ?>
-
 <script>
     
 
