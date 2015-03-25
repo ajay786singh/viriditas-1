e04f20e268f8e7f1352f4f2f8b442e2a9510180f
ajay <ajay786singh@gmail.com>
2015-03-25 14:36:21 +0530

    blog page add

--- wordpress/wp-content/themes/viriditas/archive.php
+++ wordpress/wp-content/themes/viriditas/archive.php
@@ -13,22 +13,26 @@ get_header(); ?>
     query_posts($args);
 ?>
 
-<section role="main">
+<section role="content">
     
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="archive">
             <article>
                 <div class="inner">
                     <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>                    
-                    <p><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
                     <small><?php the_author_posts_link(); ?>, <?php echo get_the_date(); ?> in <?php echo get_the_category_list(' / '); ?></small>  
+					<p><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
                 </div>
             </article>
         </div>
     
     <?php endwhile; endif; ?>
-  
+	<div class="archive">
+		<article>
+			<div class="inner">
+				<?php pagination(); ?>
+			</div>
+		</article>
+	</div>			
 </section>
-
-<?php pagination(); ?>
 <?php get_footer(); ?>
