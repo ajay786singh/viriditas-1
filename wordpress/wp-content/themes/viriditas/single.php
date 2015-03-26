b7fa6ca5186b27ef08c10e2340cd04ba0602da81
ajay <ajay786singh@gmail.com>
2015-03-26 05:33:40 +0530

    fixes #71

--- wordpress/wp-content/themes/viriditas/single.php
+++ wordpress/wp-content/themes/viriditas/single.php
@@ -3,16 +3,18 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <section role="content">
-	<article>
-		<div class="inner">
-			<h1><?php the_title(); ?></h1>
-			<small><?php the_author_posts_link(); ?>, <?php echo get_the_date(); ?> in <?php echo get_the_category_list(' / '); ?></small>    
-			<?php the_content(); 
-				single_posts_nav();
-			?>
-			
-		</div>
-	</article>
+	<div class="container">
+		<article>
+			<div class="inner">
+				<h1><?php the_title(); ?></h1>
+				<small><?php the_author_posts_link(); ?>, <?php echo get_the_date(); ?> in <?php echo get_the_category_list(' / '); ?></small>    
+				<?php the_content(); 
+					single_posts_nav();
+				?>
+				
+			</div>
+		</article>
+	</div>
 </section>
 
 <?php endwhile; endif; ?>
