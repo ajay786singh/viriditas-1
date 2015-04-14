ee4aaee59a7a9cd1a47ada4c0417671b25b64069
ajay <ajay786singh@gmail.com>
2015-04-14 17:42:42 +0530

    monograph template code update

--- wordpress/wp-content/themes/viriditas/single-monograph.php
+++ wordpress/wp-content/themes/viriditas/single-monograph.php
@@ -24,12 +24,33 @@
     <div class="container">
 		<?php the_title("<h4>","</h4>");?>
 		<?php 
+			$monograph_id=$post->ID;
 			$composition = get_post_meta($post->ID,'_monograph_details_composition',true);
 			$preparation = get_post_meta($post->ID,'_monograph_details_preparation',true);
 			$synthesis = get_post_meta($post->ID,'_monograph_details_synthesis',true);
 			$contradictions = get_post_meta($post->ID,'_monograph_details_contradictions',true);
 			$dosage = get_post_meta($post->ID,'_monograph_details_dosage',true);
 			$synergy = get_post_meta($post->ID,'_monograph_details_synergy',true);
+			$product_ids="";
+			$indications="";
+			$actions="";
+			$product_page_url=get_bloginfo('url')."/products"; 
+			$args = array(
+				'showposts' => -1,
+				'post_type' => 'product',
+				'meta_key' => '_product_details_monograph'
+			);
+			$products = new WP_Query($args);
+			if($products->have_posts()):while($products->have_posts()):$products->the_post();
+				$monograph=get_post_meta($post->ID,'_product_details_monograph',true);
+					if(in_array($monograph_id,$monograph)) {
+						$product_ids[]=$post->ID;
+					}
+			endwhile;endif;wp_reset_query();			
+			if(count($product_ids)>0) {
+				$indications = wp_get_object_terms( $product_ids, 'indication' );
+				$actions = wp_get_object_terms( $product_ids, 'actions' );
+			}
 			if($composition!=-1) {
 				echo "<section class='column-7'>";
 					echo "<h5>Composition</h5>";
@@ -54,7 +75,17 @@
 					echo apply_filters('the_content', $synthesis);
 				echo "</section>";	
 			}
-			
+			if($indications) {
+				echo "<section class='column-7'>";
+					echo "<h5>Indications</h5>";
+					echo "<ul class='list'>";	
+						foreach($indications as $indication) {
+							$indication_url=$product_page_url."?pi=".$indication->term_id;
+							echo "<li><a href='".$indication_url."'>".$indication->name."</a></li>";
+						}
+					echo "</ul>";	
+				echo "</section>";	
+			}
 			if($contradictions) {
 				echo "<section class='column-7'>";
 					echo "<h5>Contradictions, Warnings and Interactions</h5>";
@@ -73,6 +104,17 @@
 				echo "<section class='column-7'>";
 					echo "<h5>Synergy & Compounding</h5>";
 					echo apply_filters('the_content', $synergy);
+				echo "</section>";	
+			}
+			if($actions) {
+				echo "<section class='column-7'>";
+					echo "<p>If you have suggestions do not fit your needs, you may continue on linking with the catalogue. For additional symptoms & conditions link first to the system involved, select the actions required and then choose an herb from the action category.</p>";
+					echo "<ul class='list'>";	
+						foreach($actions as $action) {
+							$action_url=$product_page_url."?pi=".$action->term_id;
+							echo "<li><a href='".$action_url."'>".$action->name."</a></li>";
+						}
+					echo "</ul>";
 				echo "</section>";	
 			}
 		?>
