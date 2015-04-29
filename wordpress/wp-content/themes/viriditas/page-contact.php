a802ecb30d41706e8fee5eda043d645a1967a4a8
ajay <ajay786singh@gmail.com>
2015-04-29 13:17:17 +0530

    fix #166

--- wordpress/wp-content/themes/viriditas/page-contact.php
+++ wordpress/wp-content/themes/viriditas/page-contact.php
@@ -1,24 +1,20 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<section role="banner" style="background-image:url('<?php bloginfo('template_url' ); ?>/dist/images/contact-banner.jpg');">
-    <div class="container contact">
-        <div class="banner-content">
-			<div class="secondary">
-				<?php 
-					$map_url="https://www.google.co.in/maps/place/2775+Dundas+St+W,+Toronto,+ON+M6P+1Y4,+Canada/@43.6652469,-79.4618064,17z/data=!3m1!4b1!4m2!3m1!1s0x882b3422899970ad:0xf3a8f7a3d3db7a62?hl=en";
-				?>
-				<h4>Viriditas</h4>
-				<p>2775 Dundas Street West</p>
-				<p>Toronto, ON M6P 1Y5 <a href="<?php echo $map_url;?>" title="View on map" class="view-map" target="_blank"><img src="<?php bloginfo('template_url');?>/dist/images/map-icon.png" width="16"></a></p>
-			</div>
-        </div>
-    </div>
+<section role="banner" class="store-map">
+    <input type="hidden" id="location" value="2775+Dundas+St+W,+Toronto,+ON+M6P+1Y4,+Canada">
+	<div id="map"></div>
 </section>
 
 <section role="contact-content">
 	<article class="container contact">
 		<div class="secondary">
+			<?php 
+				$map_url="https://www.google.co.in/maps/place/2775+Dundas+St+W,+Toronto,+ON+M6P+1Y4,+Canada/@43.6652469,-79.4618064,17z/data=!3m1!4b1!4m2!3m1!1s0x882b3422899970ad:0xf3a8f7a3d3db7a62?hl=en";
+			?>
+			<h2>Viriditas</h2>
+			<p>2775 Dundas Street West</p>
+			<p>Toronto, ON M6P 1Y5 <a href="<?php echo $map_url;?>" title="View on map" class="view-map" target="_blank"><img src="<?php bloginfo('template_url');?>/dist/images/map-icon.png" width="16"></a></p>
 			<hr>
 			<p><span>p:</span> 416-767-3427</p>	
 			<p><span>f:</span> 416-767-1215</p>	
---
+++ wordpress/wp-content/themes/viriditas/src/js/custom/map.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+	if($(".store-map").length) {
+		var window_height=$(window).height();
+		$(".store-map").height(window_height/2);
+		var geocoder =  new google.maps.Geocoder();
+		var location=$('#location').val();
+		geocoder.geocode( { 'address': location}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var zoom_level=8;
+				var center=new google.maps.LatLng(results[0].geometry.location.lat(),results[0].geometry.location.lng());
+				$('#map').gmap('option', 'center', center);
+				$('#map').gmap('option', 'zoom', Number(zoom_level));
+				$('#map').gmap('addMarker', { 'bounds':false, 'position': center });
+			}
+		});
+	}
+});
