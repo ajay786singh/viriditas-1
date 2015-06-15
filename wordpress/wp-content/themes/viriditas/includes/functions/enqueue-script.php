c5d45fdce0e3a97c70ebb2de7acc487d7d614178
ajay <ajay786singh@gmail.com>
2015-06-15 20:46:48 +0530

    fix https link issues

--- wordpress/wp-content/themes/viriditas/includes/functions/enqueue-script.php
+++ wordpress/wp-content/themes/viriditas/includes/functions/enqueue-script.php
@@ -6,7 +6,7 @@ function my_scripts() {
 	wp_deregister_script('jquery');
 	wp_register_script('jquery', includes_url(). 'js/jquery/jquery.js', null, '',false);
 	wp_enqueue_script('jquery');
-	wp_enqueue_script( 'map', 'http://maps.google.com/maps/api/js?sensor=true', array(), '1.0.0', true);
+	wp_enqueue_script( 'map', '//maps.google.com/maps/api/js?sensor=true', array(), '1.0.0', true);
 	wp_enqueue_script( 'app', get_template_directory_uri() . '/dist/js/app.min.js', array(), '', false);
 	
 }
