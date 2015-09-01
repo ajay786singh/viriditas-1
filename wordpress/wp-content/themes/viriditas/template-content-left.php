ea19b34661e4558552395a6de68952a0b4769469
ajay <ajay786singh@gmail.com>
2015-09-01 11:46:21 +0530

    fix #424

---
+++ wordpress/wp-content/themes/viriditas/template-content-left.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+* Template Name: Page Content left Align
+*/
+get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<section role="content">
+    <div class="container">
+		<div class="content-grid content-left">
+			<?php the_title("<h1>","</h1>");?>
+			<?php the_content();?>
+		</div>	
+	</div>
+</section>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
