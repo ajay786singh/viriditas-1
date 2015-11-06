7c3de2d1d12704fb314df84fb7ea3427a444bb34
ajay <ajay786singh@gmail.com>
2015-11-06 13:27:25 +0530

    fix readmore js

--- custom/themes/viriditas/src/js/custom/readmore.js
+++ custom/themes/viriditas/src/js/custom/readmore.js
@@ -1,16 +1,12 @@
 jQuery(document).ready(function($) {
-	// $('.post-course .content').each(function(){
-		// alert($(this).height());
-	// });
-	
 	$('.post-course .content').readmore({
 	  speed: 75,
 	  moreLink: '<a href="#" class="more-link">Learn more</a>',
 	  lessLink: '<a href="#" class="more-link less">Show less</a>'
-	});
+	}); 
 	$('.team-member .team-content').readmore({
 	  speed: 75,
 	  moreLink: '<a href="#" class="more-link">Learn more</a>',
-	  lessLink: '<a href="#" class="more-link less">Show less</a>' 
+	  lessLink: '<a href="#" class="more-link less">Show less</a>'
 	});
 });
