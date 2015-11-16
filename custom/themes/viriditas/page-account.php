e15936f1f3c27532a86c4c19ad4f1fcda5669efd
ajay <ajay786singh@gmail.com>
2015-11-16 14:05:34 +0530

    my acocunt section style changes

--- custom/themes/viriditas/page-account.php
+++ custom/themes/viriditas/page-account.php
@@ -18,35 +18,10 @@
 						echo "<h1>Account Setup</h1>";
 					}
 					the_content();
-				} else {
-					echo "<h1>Welcome</h1>";
-			?>
-				<p>You are already registered with us.</p>
-				<p>Please wait, you will be redirecting to profile page in <span id="countdown">5</span> seconds .</p>
-				<script type="text/javascript">
-					(function () {
-						var timeLeft = 5,
-							cinterval;
-
-						var timeDec = function (){
-							timeLeft--;
-							document.getElementById('countdown').innerHTML = timeLeft;
-							if(timeLeft === 0){
-								clearInterval(cinterval);
-								window.location="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>";
-							}
-						};
-						cinterval = setInterval(timeDec, 1000);
-					})();
-				</script>
-			<?php		
 				}
 			?>
-			
 		</article>	
 	</div>
 </section>
-
 <?php endwhile; endif; ?>
-
 <?php get_footer(); ?>
