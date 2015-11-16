b08603c4d1c88fbb61832506b8c9706d9d66eec0
ajay <ajay786singh@gmail.com>
2015-11-16 20:43:30 +0530

    redirest message change

--- custom/themes/viriditas/src/js/custom/woocommerce-order-again.js
+++ custom/themes/viriditas/src/js/custom/woocommerce-order-again.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
 			success: function(html) {									
 				//alert(html);
 				$('.order-again').removeClass('small-loader');
-				$('.order-again').empty().html("Your order has been added to cart again and We are redirecting you to cart page...");
+				$('.order-again').empty().html("Thanks! This order has been added to your cart. In a moment you will be re-directed to the cart page.");
 				window.location=cart_page;
 			}		
 		});
