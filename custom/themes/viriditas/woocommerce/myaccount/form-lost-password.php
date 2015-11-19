210d46aa7f9273d0f225f0719472ab91ca0b8009
ajay <ajay786singh@gmail.com>
2015-11-19 21:29:10 +0530

    fix style for login and lost password page

--- custom/themes/viriditas/woocommerce/myaccount/form-lost-password.php
+++ custom/themes/viriditas/woocommerce/myaccount/form-lost-password.php
@@ -15,26 +15,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php wc_print_notices(); ?>
 <h2><?php _e( 'Lost password', 'woocommerce' ); ?></h2>
-<form method="post" class="lost_reset_password">
+<form method="post" class="account-form lost_reset_password">
 
 	<?php if( 'lost_password' == $args['form'] ) : ?>
 		
 		<p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p>
-
-		<p class="form-row form-row-first"><label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="user_login" id="user_login" /></p>
+		<div class="form-group">
+			<label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label> 
+			<input class="input-text" type="text" name="user_login" id="user_login" />
+		</div>
 
 	<?php else : ?>
 
 		<p><?php echo apply_filters( 'woocommerce_reset_password_message', __( 'Enter a new password below.', 'woocommerce') ); ?></p>
 
-		<p class="form-row form-row-first">
+		<div class="form-group">
 			<label for="password_1"><?php _e( 'New password', 'woocommerce' ); ?> <span class="required">*</span></label>
 			<input type="password" class="input-text" name="password_1" id="password_1" />
-		</p>
-		<p class="form-row form-row-last">
+		</div>
+		<div class="form-group">
 			<label for="password_2"><?php _e( 'Re-enter new password', 'woocommerce' ); ?> <span class="required">*</span></label>
 			<input type="password" class="input-text" name="password_2" id="password_2" />
-		</p>
+		</div>
 
 		<input type="hidden" name="reset_key" value="<?php echo isset( $args['key'] ) ? $args['key'] : ''; ?>" />
 		<input type="hidden" name="reset_login" value="<?php echo isset( $args['login'] ) ? $args['login'] : ''; ?>" />
@@ -43,10 +45,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="clear"></div>
 
-	<p class="form-row">
+	<div class="form-group">
 		<input type="hidden" name="wc_reset_password" value="true" />
 		<input type="submit" class="button" value="<?php echo 'lost_password' == $args['form'] ? __( 'Reset Password', 'woocommerce' ) : __( 'Save', 'woocommerce' ); ?>" />
-	</p>
+	</div>
 
 	<?php wp_nonce_field( $args['form'] ); ?>
 
