6c9c5cf9cde7e5c8d4ea5b3b72a9994f01f87320
ajay <ajay786singh@gmail.com>
2015-11-23 09:52:37 +0530

    fix #629

--- custom/themes/viriditas/includes/functions/enqueue-script.php
+++ custom/themes/viriditas/includes/functions/enqueue-script.php
@@ -11,4 +11,13 @@ function my_scripts() {
 
 }
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
+
+/* Enqueue Script for datepick function for appointment+ plugin  */
+function appointment_enqueue_admin_script( $hook ) {
+    if ( 'user-edit.php' != $hook ) {
+        return;
+    }
+    wp_enqueue_script( 'appintment-datepick', plugin_dir_url('appointments').'appointments/js/jquery.datepick.min.js', array(), '1.0' );
+}
+add_action( 'admin_enqueue_scripts', 'appointment_enqueue_admin_script' );
 ?>
