343a3c98c5c254fb91e50df131f70a37ef25c806
ajay <ajay786singh@gmail.com>
2015-12-05 20:10:58 +0530

    style fix

--- custom/themes/viriditas/includes/functions/enqueue-script.php
+++ custom/themes/viriditas/includes/functions/enqueue-script.php
@@ -20,4 +20,22 @@ function appointment_enqueue_admin_script( $hook ) {
     wp_enqueue_script( 'appintment-datepick', plugin_dir_url('appointments').'appointments/js/jquery.datepick.min.js', array(), '1.0' );
 }
 add_action( 'admin_enqueue_scripts', 'appointment_enqueue_admin_script' );
+
+/* Dequeue Woocommerce Scripts from other pages */
+function conditionally_load_woc_js_css(){
+	if( function_exists( 'is_woocommerce' ) ){
+		# Only load CSS and JS on Woocommerce pages   
+		if(! is_woocommerce() && ! is_cart() && ! is_checkout() ) { 		
+			## Dequeue scripts.
+			wp_dequeue_script('woocommerce'); 
+			wp_dequeue_script('wc-add-to-cart'); 
+			wp_dequeue_script('wc-cart-fragments');				
+			## Dequeue styles.	
+			wp_dequeue_style('woocommerce-general'); 
+			wp_dequeue_style('woocommerce-layout'); 
+			wp_dequeue_style('woocommerce-smallscreen'); 		
+		}
+	}	
+}
+add_action( 'wp_enqueue_scripts', 'conditionally_load_woc_js_css' );
 ?>
