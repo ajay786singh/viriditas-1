94cdadee067fa9cf6f7d624904bf7a6cca84d3f4
ajay <ajay786singh@gmail.com>
2016-01-13 10:23:32 +0530

    fix #729

--- custom/themes/viriditas/template-appointment-forms.php
+++ custom/themes/viriditas/template-appointment-forms.php
@@ -8,19 +8,21 @@ get_header(); ?>
 
 <section role="content">
     <div class="container">
-		<div class="column-9 border-right appointment-form-content">
-			<?php the_title("<h1>","</h1>");?>
-			<?php the_content();?>
+		<div class="content-grid content-left">
+			<div class="column-8 border-right appointment-form-content">
+				<?php the_title("<h1>","</h1>");?>
+				<?php the_content();?>
+			</div>
+			<?php 
+				$pdf=get_post_meta(get_the_ID(),'_appointment_block_appointment_pdf',true);
+				if($pdf!='') {
+			?>
+			<aside class="column-4 sidebar">
+				<h5>Prefer to fill this out by hand?</h5>
+				<a href="<?php echo $pdf;?>" target="_blank" class="icon-download">Download Form</a>
+			</aside>	
+			<?php } ?>
 		</div>
-		<?php 
-			$pdf=get_post_meta(get_the_ID(),'_appointment_block_appointment_pdf',true);
-			if($pdf!='') {
-		?>
-		<aside class="column-3 sidebar">
-			<h5>Prefer to fill this out by hand?</h5>
-			<a href="<?php echo $pdf;?>" target="_blank" class="icon-download">Download Form</a>
-		</aside>	
-		<?php } ?>
 	</div>
 </section>
 
